577c8c34589b23565a0fffa2b35acdd534255f53
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-01-19 19:50:56 +0200

    task1

---
+++ task1.py
@@ -0,0 +1,9 @@
+sales_amounts = [1500, 2200, 1800, 2000, 2500, 2100, 1900]
+
+total_sales = sum(sales_amounts)
+
+average_sales = total_sales / len(sales_amounts)
+
+print("total sales:", total_sales)
+print("average sales:", average_sales)
+
